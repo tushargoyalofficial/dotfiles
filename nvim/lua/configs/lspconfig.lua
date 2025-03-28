96b7a00114137eebf04076b8dc7f82cd0fce28e9
TUSHAR GOYAL <tushargoyal@TUSHARs-MacBook-Air.local>
2025-03-29 02:44:48 +0530

    Update nvim to use pyright for python

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -12,6 +12,7 @@ local servers = {
   "html",
   "jsonls",
   "prismals",
+  "pyright",
   "ruff",
   "tailwindcss",
   "ts_ls",
@@ -33,3 +34,14 @@ lspconfig.ts_ls.setup {
   on_init = nvlsp.on_init,
   capabilities = nvlsp.capabilities,
 }
+
+-- Disable hover in Ruff to avoid conflicts with Pyright
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client and client.name == "ruff" then
+      client.server_capabilities.hoverProvider = false
+    end
+  end,
+})
